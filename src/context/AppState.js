6ebbc221da09e7628c4c066f61116ad03123e585
tulipekoni <34221969+tulipekoni@users.noreply.github.com>
2022-12-25 02:17:10 +0200

    accidental npm package.lock file removed

--- src/context/AppState.js
+++ src/context/AppState.js
@@ -1,4 +1,4 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useState } from "react";
 import { func } from "../constraints";
 export const AppContext = createContext();
 export const AppContextProvider = ({ children }) => {
